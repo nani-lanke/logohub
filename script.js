09ae5f0cdd136d8d29a47149bba139b2ea6c971b
nani-lanke <lanke0612@gmail.com>
2026-05-04 21:31:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ const companies = [
     box        : 76,
     name       : 'LogoHub',
     shortName  : 'LH',
-    icon       : '/screenone/box38.png',
+    icon       : '/icons/screenone/box38.png',
     category   : 'entertainmentAndInformationService',
     serviceType: 'Information & Directory Service',
     links: {
