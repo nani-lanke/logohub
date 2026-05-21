7ee5c105baa51b85edebd8250b715d41a9c16c47
nani-lanke <lanke0612@gmail.com>
2026-05-21 21:21:15 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ const companies = [
     box        : 76,
     name       : 'LogoHub',
     shortName  : 'LH',
-    icon       : '/icons/screenone/box38.png',
+    icon       : '/screenone/box38.png',
     category   : 'entertainmentAndInformationService',
     serviceType: 'Information & Directory Service',
     links: {
